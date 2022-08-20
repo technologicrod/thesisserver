eb838764880ce94368ee0bef184aadb6adb257aa
technologicrod <matchon.rodwell@gmail.com>
2022-08-21 02:51:27 +0800

    Third commit. can now passed id to get data in mysql

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -38,9 +39,7 @@ app.get("/", (req, res) => {
     res.send("hello world")
 })
 
-app.listen(3001, () => {
-    console.log("running on port 3001");
-})
+
 app.get("/employeelist", (req,res) => {
     const sqlSelect = "SELECT * FROM employeeprofile;"
     db.query(sqlSelect, (err, result) =>{
@@ -74,4 +73,56 @@ app.post("/employeelistadd", upload.array('images', 2), (req,res) => {
         console.log(result);
     })
     
+})
+
+app.get('/employeelistpositionhistory/:employeeid', (req, res) => {
+    const employeeid = req.params.employeeid;
+    const sqlemployeeview = "SELECT * from employeeprofile WHERE employeeid = ?;"
+    db.query(sqlemployeeview, employeeid, (err, result) =>{
+        res.json(result);
+        console.log(result)
+    })
+})
+
+
+//crud test
+app.get("/api/get", (req,res) => {
+    const sqlSelect = "SELECT * FROM movie_reviews;"
+    db.query(sqlSelect, (err, result) =>{
+        res.send(result);
+    })
+})
+
+app.post("/api/insert", (req,res) => {
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?);"
+    db.query(sqlInsert,[movieName, movieReview], (err, result) =>{
+        console.log(result);
+    })
+    
+})
+
+app.delete('/api/delete/:movieName', (req,res) => {
+    const name = req.params.movieName;
+    const sqlDelete = "DELETE from movie_reviews WHERE movieName = ?";
+    db.query(sqlDelete, name, (err, result) => {
+        if (err) console.log(err);
+    });
+
+})
+
+app.put('/api/update', (req,res) => {
+    const name = req.body.movieName;
+    const review = req.body.movieReview;
+    const sqlUpdate = "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
+    db.query(sqlUpdate, [review, name], (err, result) => {
+        if (err) console.log(err);
+    });
+
+})
+app.listen(3001, () => {
+    console.log("running on port 3001");
 })
